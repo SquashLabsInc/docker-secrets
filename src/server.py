9b4ac2d03b129b356b5bef5e85912a0dfd5b3873
Evandro Miquelito <evandro@pixbuffer.com>
2022-08-17 13:35:13 -0600

    example app with docker secrets

---
+++ src/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+import threading
+import time
+import logging
+
+app = Flask(__name__)
+
+
+@app.before_first_request
+def activate_job():
+    def run_job():
+        while True:
+            time.sleep(0.25)
+
+    thread = threading.Thread(target=run_job)
+    thread.start()
+
+
+@app.route('/')
+def index():
+    return '<body>Hello world. <a href="/about/">About this page</a>.</body>'
+
+
+@app.route('/about')
+def about():
+    logging.warning(request.referrer)
+    return '<body>This is the about page</body>'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
